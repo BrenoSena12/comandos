2a63ffee98353d83755aa045fbad0ad5df438800
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-03-20 20:46:17 -0300

    operacoes matematicas

---
+++ Operacoes_Matematicas/operacoes.java
@@ -0,0 +1,8 @@
+//RAIZ QUADRADA
+a = Math.sqrt(x); //variavel a recebe a raiz quadrada de x
+
+//NUMERO ELEVADO
+a = Math.pow(x,y) //variavel a recebe o resultado de x elevado a y
+
+//VALOR ABSOLUTO
+a = Math.abs(x); //variavel a recebe o valor absoluto de x
