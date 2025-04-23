b2c420e0032e016c9fb29986e1cc29cc97b80662
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-04-23 13:05:01 -0300

    ex009

--- EXERCICIOS/VETORES/ex009.java
+++ EXERCICIOS/VETORES/ex009.java
@@ -1,4 +1,87 @@
 /*
 A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0 a 9.
 Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto.
-/*
+*/
+
+package entities;
+
+public class Aluguel {
+
+	private String nome;
+	private String email;
+
+	public Aluguel() {
+	}
+
+	public Aluguel(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return nome + ", " + email;
+	}
+
+}
+
+//*********************************************************************************************** */
+package aplication;
+
+import java.util.Scanner;
+
+import entities.Aluguel;
+
+public class AluguelProgram {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		Aluguel[] vetor = new Aluguel[10];
+
+		System.out.print("Quantas pessoas vão alugar? ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Aluguel #" + i + ":");
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Quarto: ");
+			int numeroQuarto = sc.nextInt();
+
+			vetor[numeroQuarto] = new Aluguel(name, email);
+		}
+
+		System.out.println();
+		System.out.println("QUARTOS ALUGADOS:");
+
+		for (int i = 0; i < 10; i++) {
+			if (vetor[i] != null) {
+				System.out.println(i + ": " + vetor[i]);
+			}
+
+		}
+
+	}
+
+}
