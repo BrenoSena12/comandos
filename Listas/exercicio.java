e212d651f26f394418d4fa667037428eb53a8b0c
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-05-07 16:40:32 -0300

    terminei

--- Listas/exercicio.java
+++ Listas/exercicio.java
@@ -5,7 +5,7 @@ public class Funcionario {
 	private Integer id;
 	private String nome;
 	private Double salario;
-	
+
 	// CONSTRUTOR
 	public Funcionario() {
 	}
@@ -40,16 +40,16 @@ public class Funcionario {
 	public void setSalario(Double salario) {
 		this.salario = salario;
 	}
-	
+
 	// METODOS
-	
+
 	public void aumentoSalarial(double porcentagem) {
 		salario = (salario * porcentagem / 100) + salario;
 	}
-	
+
 }
 
-/*********************************** */
+/********************************************************************** */
 package application;
 
 import java.util.ArrayList;
@@ -64,15 +64,15 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Funcionario funcionario = new Funcionario();
-		
+
 		List<Funcionario> listaFuncionario = new ArrayList<>();
-		
+
 		System.out.print("QUANTIDADE DE FUNCIONARIOS: ");
 		int n = sc.nextInt();
-		
-		for(int indice = 0; indice < n; indice++) {
+
+		for (int indice = 0; indice < n; indice++) {
 			System.out.println();
 			System.out.println("FUNCIONARIO #" + (indice + 1));
 			System.out.print("Id : ");
@@ -85,33 +85,48 @@ public class Program {
 
 			listaFuncionario.add(funcionario = new Funcionario(id, nome, salario));
 		}
-		
+
 		System.out.println();
-		
+
 		// TRÁS O DADO DA LISTA
-		// System.out.println(listaFuncionario.get(0).getNome());  
-		
+		// System.out.println(listaFuncionario.get(0).getNome());
+
 		System.out.print("DIGITE O ID PARA O AUMENTO SALARIAL: ");
 		double aumento = sc.nextDouble();
-		
+
 		System.out.print("PORCENTAGEM DE AUMENTO: ");
 		double porcentagem = sc.nextDouble();
-		
-		listaFuncionario.indexOf(aumento).funcionario.aumentoSalarial(porcentagem);
-		
-		//funcionario.aumentoSalarial(porcentagem);
-		
+
+		Funcionario func = null; // ESSA VARIAVEL IRÁ GUARDAR O ID, NOME, SALARIO
+
+		// irá filtrar um á um até achar o id selecionado. Depois de encontradado vai
+		// guardar os dados na variavel func, o id, nome e salario.
+		for (Funcionario f : listaFuncionario) {
+			if (f.getId() == aumento) {
+				func = f;
+				break;
+			}
+		}
+		// como o func contem os dados que precisamos é só passar os comandos.
+		if (func != null) {
+			func.aumentoSalarial(porcentagem);
+		} else {
+			System.out.println("Funcionário com ID " + aumento + " não encontrado.");
+		}
+
 		System.out.println();
-		
+
 		System.out.println("LISTA DE FUNCIONARIOS: ");
-		
-		for(Funcionario f : listaFuncionario) {
+
+		for (Funcionario f : listaFuncionario) {
 			System.out.print(f.getId() + ", ");
 			System.out.print(f.getNome() + ", ");
-			System.out.printf("%.2f", f.getSalario() );
+			System.out.printf("%.2f", f.getSalario());
 			System.out.println();
 		}
-		
+
+		System.out.println(func.getNome());
+
 		sc.close();
 	}
 }
