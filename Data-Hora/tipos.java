6c547fc8d04259ac4ec2a163066c6d0dcafa6444
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-05-10 11:29:21 -0300

    update

--- Data-Hora/tipos.java
+++ Data-Hora/tipos.java
@@ -1,24 +1,24 @@
 /******************************  DATA E HORA  ******************************/
 
 // INSTANCIAR COM A DATA DE AGORA ( ano / mes / dia )
-	LocalDate dataAgroa = LocalDate.now();
-	System.out.println(dataAgroa);
+LocalDate dataAgroa = LocalDate.now();
+System.out.println(dataAgroa);
 
 // INSTANCIAR COM A DATA E HORA DE AGORA ( ano / mes / dia - hora)
-    LocalDateTime dataHoraAgroa = LocalDateTime.now();
-	System.out.println(dataHoraAgroa);
+LocalDateTime dataHoraAgroa = LocalDateTime.now();
+System.out.println(dataHoraAgroa);
 
 // DATA HORA GLOBAL - LONDRES ( ano / mes / dia T-hora-Z)
-	Instant dataHoraAgoraLondres = Instant.now();
-	System.out.println(dataHoraAgoraLondres);
+Instant dataHoraAgoraLondres = Instant.now();
+System.out.println(dataHoraAgoraLondres);
 
 // TRANSFORMAR TEXTO EM DATA
-	LocalDate textoEmData = LocalDate.parse("2025-05-09");
-	System.out.println(textoEmData);
+LocalDate textoEmData = LocalDate.parse("2025-05-09");
+System.out.println(textoEmData);
 
 // TRANSFORMAR TEXTO EM DATA HORA
-	LocalDateTime textoEmDataHora = LocalDateTime.parse("2025-05-09T01:30:36");
-	System.out.println(textoEmDataHora);
+LocalDateTime textoEmDataHora = LocalDateTime.parse("2025-05-09T01:30:36");
+System.out.println(textoEmDataHora);
 
 // TRANSFORMAR TEXTO CUSTOMIZADO E DATA 
     DateTimeFormatter formato1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
