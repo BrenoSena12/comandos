40a670691cf1718f49f15c653e0551e7d03451b4
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-06-04 21:14:37 -0300

    herança

--- Herança/program.java
+++ Herança/program.java
@@ -7,7 +7,7 @@ import entities.SavingsAccount;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Account acc = new Account(1001, "Alex", 0.0);
 		BusinnesAccount bacc = new BusinnesAccount(1002, "Maria", 0.0, 500.0);
 		
@@ -19,3 +19,18 @@ public class Program {
 		// DOWNCASTING = Converter um objeto da super-classe para uma sub-classe
 		BusinnesAccount acc4 = (BusinnesAccount)acc2;
 		acc4.loan(100.0);
+		
+		if(acc3 instanceof BusinnesAccount) {
+			BusinnesAccount acc5 = (BusinnesAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan");
+		}
+		
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();;
+			System.out.println("Update!");
+		}
+		
+	}
+}
