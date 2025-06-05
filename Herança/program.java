f6e63ee954431022601e8a374bf11dd6521648c0
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-06-04 21:14:21 -0300

    herança

--- Herança/program.java
+++ Herança/program.java
@@ -7,3 +7,15 @@ import entities.SavingsAccount;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinnesAccount bacc = new BusinnesAccount(1002, "Maria", 0.0, 500.0);
+		
+		// UPCASTING = é pegar um objeto do tipo BusinnesAccount e atribuir a uma varivel do tipo Account
+		Account acc1 = bacc;
+		Account acc2 = new BusinnesAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		// DOWNCASTING = Converter um objeto da super-classe para uma sub-classe
+		BusinnesAccount acc4 = (BusinnesAccount)acc2;
+		acc4.loan(100.0);
