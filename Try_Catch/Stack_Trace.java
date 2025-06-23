534a6910bb5edb0e02f05e65efc2a9f466a649d5
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2025-06-23 19:18:35 -0300

    Stack trace

---
+++ Try_Catch/Stack_Trace.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		method1();
+		
+		System.out.println("fim do programa");
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Erro posição invalida");
+			e.printStackTrace();
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Erro de entrada");
+		}
+		
+		sc.close();
+		System.out.println("***METHOD2 END***");
+	}
+}
